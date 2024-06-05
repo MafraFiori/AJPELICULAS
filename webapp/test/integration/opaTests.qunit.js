805e019c6d84192d0aebfe70b6a40ab313e9ebcb
MafraFiori <133063021+MafraFiori@users.noreply.github.com>
2024-06-05 16:43:42 -0300

    PRIMEIRO DEPLOY

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["ajpeliculas/catalogo/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"ajpeliculas/catalogo/test/unit/controller/Home.controller"
+], function () {
+	"use strict";
+});
